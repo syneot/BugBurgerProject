4f169e623333e07a6b796bdb61fafac00b5a0e86
syneot <syneot@gmail.com>
2017-10-25 14:57:45 +0200

    change routes

--- include/footer.php
+++ include/footer.php
@@ -2,13 +2,13 @@
 
     <div class="row">
         <div class="col-md-3 col-xs-12">
-            <a href="../index.php">Bug Burger</a>
+            <a href="/BugBurgerProject/index.php">Bug Burger</a>
         </div>
         <div class="col-md-3 col-xs-12">
-            <a href="../pages/plats.php">Nos Burgers</a>
+            <a href="/BugBurgerProject/pages/plats.php">Nos Burgers</a>
         </div>
         <div class="col-md-3 col-xs-12">
-            <a href="../pages/restaurants.php">Nos Restaurants</a>
+            <a href="/BugBurgerProject/pages/restaurants.php">Nos Restaurants</a>
         </div>
         <div class="col-md-3 col-xs-12">
             <p>Copyright WCS 2017</p>
@@ -16,16 +16,16 @@
     </div>
     <div class="row">
         <div class="col-md-3 col-xs-12">
-            <a href="/bug_burger/pages/equipe.php">Notre Equipe</a>
+            <a href="/BugBurgerProject/pages/equipe.php">Notre Equipe</a>
         </div>
         <div class="col-md-3 col-xs-12">
-            <a href="/bug_burger/pages/faq.php">Faq</a>
+            <a href="/BugBurgerProject/pages/faq.php">Faq</a>
         </div>
         <div class="col-md-3 col-xs-12">
-            <a href="/bug_burger/pages/contact.php">Contactez-Nous</a>
+            <a href="/BugBurgerProject/pages/contact.php">Contactez-Nous</a>
         </div>
         <div class="col-md-3 col-xs-12">
-            <p><a href="/bug_burger/pages/mentions-legales.php">Mentions Légales</a></p>
+            <p><a href="/BugBurgerProject/pages/mentions-legales.php">Mentions Légales</a></p>
         </div>
     </div>
 </div>
--- include/header.php
+++ include/header.php
@@ -10,18 +10,18 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a href="/Lyon-project1-MSF/index.php">
+                    <a href="/BugBurgerProject/index.php">
                         <img class='navbar-brand' src="http://img1.imagilive.com/0917/Logoreal.png" alt="logo">
                     </a>
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/Lyon-project1-MSF/index.php">Bug Burger</a></li>
-                        <li><a href="/Lyon-project1-MSF/pages/plats.php">Nos Burgers</a></li>
-                        <li><a href="/Lyon-project1-MSF/pages/restaurants.php">Nos Restaurants</a></li>
-                        <li><a href="/Lyon-project1-MSF/pages/equipe.php">Notre Equipe</a></li>
-                        <li><a href="/Lyon-project1-MSF/pages/faq.php">Faq</a></li>
-                        <li><a href="/Lyon-project1-MSF/pages/contact.php">Contactez-nous</a></li>
+                        <li><a href="/BugBurgerProject/index.php">Bug Burger</a></li>
+                        <li><a href="/BugBurgerProject/pages/plats.php">Nos Burgers</a></li>
+                        <li><a href="/BugBurgerProject/pages/restaurants.php">Nos Restaurants</a></li>
+                        <li><a href="/BugBurgerProject/pages/equipe.php">Notre Equipe</a></li>
+                        <li><a href="/BugBurgerProject/pages/faq.php">Faq</a></li>
+                        <li><a href="/BugBurgerProject/pages/contact.php">Contactez-nous</a></li>
                     </ul>
                 </div>
             </div>
