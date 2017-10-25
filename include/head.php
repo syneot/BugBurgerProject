5475e735692c76b5b32f133c83842b7c0d126907
syneot <syneot@gmail.com>
2017-10-25 14:13:32 +0200

    add chemins

--- include/head.php
+++ include/head.php
@@ -5,4 +5,4 @@
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
-    <link rel="stylesheet" href="/bug_burger/css/style.css">
+    <link rel="stylesheet" href="/Lyon-project1-MSF/css/style.css">
--- include/header.php
+++ include/header.php
@@ -10,18 +10,18 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a href="/bug_burger/index.php">
+                    <a href="/Lyon-project1-MSF/index.php">
                         <img class='navbar-brand' src="http://img1.imagilive.com/0917/Logoreal.png" alt="logo">
                     </a>
                 </div>
                 <div class="collapse navbar-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="/bug_burger/index.php">Bug Burger</a></li>
-                        <li><a href="/bug_burger/pages/plats.php">Nos Burgers</a></li>
-                        <li><a href="/bug_burger/pages/restaurants.php">Nos Restaurants</a></li>
-                        <li><a href="/bug_burger/pages/equipe.php">Notre Equipe</a></li>
-                        <li><a href="/bug_burger/pages/faq.php">Faq</a></li>
-                        <li><a href="/bug_burger/pages/contact.php">Contactez-nous</a></li>
+                        <li><a href="/Lyon-project1-MSF/index.php">Bug Burger</a></li>
+                        <li><a href="/Lyon-project1-MSF/pages/plats.php">Nos Burgers</a></li>
+                        <li><a href="/Lyon-project1-MSF/pages/restaurants.php">Nos Restaurants</a></li>
+                        <li><a href="/Lyon-project1-MSF/pages/equipe.php">Notre Equipe</a></li>
+                        <li><a href="/Lyon-project1-MSF/pages/faq.php">Faq</a></li>
+                        <li><a href="/Lyon-project1-MSF/pages/contact.php">Contactez-nous</a></li>
                     </ul>
                 </div>
             </div>
